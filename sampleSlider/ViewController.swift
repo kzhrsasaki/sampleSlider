266c5e685227ef540f46a61c20bef12046cb1a20
kzhrsasaki <kzhr.sasaki@gmail.com>
2017-01-23 20:12:45 +0900

    2017/01/23 iOSコース宿題

--- sampleSlider/ViewController.swift
+++ sampleSlider/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+    @IBOutlet weak var mySlider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func sliderChange(_ sender: UISlider) {
+        print(sender.value)
+        // TODO:つまみの値が0.5以上であれば「音が大きいです」それ以外は「音が小さいです」とデバッグエリアに表示してみましょう(if文)
+        if sender.value >= 0.5 {
+            print("音が大きいです")
+        } else {
+            print("音が小さいです")
+        }
     }
 
     override func didReceiveMemoryWarning() {
